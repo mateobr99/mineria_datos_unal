ed994248c6adebc14ac7a805d8a8dd147a542fda
mateobr99 <154276229+mateobr99@users.noreply.github.com>
2025-01-25 11:51:04 -0500

    aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main(): 
   st.title("Clasificación de la base de datos mnist")
-  st.marckdown("Sube una imagen para clasificar")
+  st.markdown("Sube una imagen para clasificar")
   
 if __name__=='__main__':
   main()
