076c2cc2abcb49c25d1793f3c12c367ba808d957
mateobr99 <154276229+mateobr99@users.noreply.github.com>
2025-01-28 18:24:17 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main(): 
   st.title("Clasificación de la base de datos mnist")
   st.markdown("Sube una imagen para clasificar")
-  uploaded_file = st.file_uploader("Selecciona una imagen (PNG,JPG, JEG), type = ["jpg", "png", "jpeg"])
+  uploaded_file = st.file_uploader("Selecciona una imagen (PNG,JPG, JEG:)", type = ["jpg", "png", "jpeg"])
 
 if __name__=='__main__':
   main()
