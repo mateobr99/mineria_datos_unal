a855c4035581fefe4e35316de86af222efe4fe5d
mateobr99 <154276229+mateobr99@users.noreply.github.com>
2025-01-28 18:28:51 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -1,9 +1,23 @@
 import streamlit as st
+from PIL import as Image
+from tensorflow.preprocessing.image import img_to_array
 
+def preprocess_image(image):
+  image = image.convert('L') #Convertir a blanco y negro
+  image = image.resize((28,28))
+  image_array = img_to_array(image)/255.0
+  
 def main(): 
   st.title("Clasificación de la base de datos mnist")
   st.markdown("Sube una imagen para clasificar")
-  uploaded_file = st.file_uploader("Selecciona una imagen (PNG,JPG, JEG:)", type = ["jpg", "png", "jpeg"])
+  uploaded_file = st.file_uploader("Selecciona una imagen (PNG,JPG, JEG:)", type = ["jpg", "png", "jpeg"]
+  
+  if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption = "imagen subida")
 
+
+
+                                   
 if __name__=='__main__':
   main()
